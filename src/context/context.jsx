4bd03192d7471add2aaad4deb398394a92f41c4c
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-06-17 07:26:27 +0530

    feat: implement ContextProvider for state management

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,3 +1,14 @@
 import { createContext } from "react";
 
 export const context = createContext();
+const ContextProvider = (props) => {
+    const contextValue = {
+
+    }
+    return (
+        <context.Provider value={contextValue}>
+            {props.children}
+        </context.Provider>
+    ); 
+}
+export default ContextProvider;
